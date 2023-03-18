765663e5c4af0eb8cf25c02c6e77ebbaa251519b
jh.cho <ctp102>
2023-03-18 12:28:29 +0900

    feat: 멀티 모듈 프로젝트 구축

---
+++ search-api-server/src/test/java/io/search/api/SearchApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.search.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ search-core/src/test/java/io/search/core/SearchCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.search.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
