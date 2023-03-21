a0430ca214afa68d9410f0dbceefc5a5d39dad68
jh.cho <ctp102>
2023-03-21 21:39:48 +0900

    feat: 테스트 케이스 추가

--- search-core/src/test/java/io/search/core/search/service/SearchServiceTest.java
+++ search-core/src/test/java/io/search/core/search/service/SearchServiceTest.java
@@ -8,6 +8,8 @@ import io.search.core.search.dto.SearchResultDto;
 import io.search.core.search.enums.PlatformType;
 import io.search.core.search.form.SearchForm;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,33 +25,68 @@ class SearchServiceTest extends CommonTest {
     @Autowired
     private SearchService searchService;
 
-    @Test
-    public void getHotSearchByQuery() throws Exception {
-        List<SearchDomain> searchDomain = searchService.getHotTop10Search();
-        viewJson(searchDomain);
-    }
+    @BeforeEach
+    @DisplayName("")
+    public void setUp() throws Exception {
 
-    @Test
-    public void getSearchByQueryAndPlatform() throws Exception {
         String query = "개발자";
         PlatformType platform = PlatformType.KAKAO;
-        SearchDomain searchDomain = searchService.getSearchByQueryAndPlatform(query, platform);
 
-        assertThat(searchDomain.getId()).isEqualTo(1);
+        for (int i = 10; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                SearchDomain findSearchDomain = searchService.getSearchByQueryAndPlatform(query + i, platform);
+                searchService.insertSearch(findSearchDomain, query + i, platform);
+            }
+        }
+
     }
 
     @Test
+    @DisplayName("인기검색어 상위 10개 조회 테스트")
+    public void getHotSearchByQuery() throws Exception {
+
+        List<SearchDomain> searchDomains = searchService.getHotTop10Search();
+
+        assertThat(searchDomains.size()).isLessThanOrEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("검색어와 플랫폼으로 Search 엔티티 조회 테스트")
+    public void getSearchByQueryAndPlatform() throws Exception {
+
+        String query = "개발자1";
+        PlatformType platform = PlatformType.KAKAO;
+
+        SearchDomain searchDomain = searchService.getSearchByQueryAndPlatform(query, platform);
+
+        assertThat(searchDomain).isNotNull();
+    }
+
+    @Test
+    @DisplayName("블로그 검색 테스트")
     public void searchBlog() throws Exception {
-        SearchForm searchForm = new SearchForm();
-        searchForm.setQuery("개발자");
-//        searchForm.setSort("recency");
-        searchForm.setSort("accuracy");
 
-        PagingForm pagingForm = new PagingForm();
+        String query = "개발자";
+        SearchForm searchForm = new SearchForm(query);
 
-        List<SearchResultDto> searchResults = searchService.searchBlog(searchForm, pagingForm);
+        List<SearchResultDto> searchResults = searchService.searchBlog(searchForm, new PagingForm());
 
-        viewJson(searchResults);
+        assertThat(searchResults.size()).isGreaterThan(0);
+    }
+
+    @Test
+    @DisplayName("같은 검색어로 블로그 검색 후 count 증가 테스트")
+    public void SearchCount() throws Exception {
+
+        String query = "개발자";
+        SearchForm searchForm = new SearchForm(query);
+
+        searchService.searchBlog(searchForm, new PagingForm());
+        searchService.searchBlog(searchForm, new PagingForm());
+
+        SearchDomain searchDomain = searchService.getSearchByQueryAndPlatform(query, PlatformType.KAKAO);
+
+        assertThat(searchDomain.getCount()).isEqualTo(2);
     }
 
 }
